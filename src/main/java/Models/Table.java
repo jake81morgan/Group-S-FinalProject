475caaaf45eaa9da059ca5b9bd4793d1f9f647ca
joeyg <joeyg@Joey_Desktop>
2024-04-22 18:24:34 -0500

    Added featurs to Table.java as well as TableReservationService
    Renamed ReservationController to TableController

--- src/main/java/Models/Table.java
+++ src/main/java/Models/Table.java
@@ -4,4 +4,38 @@ public class Table {
 	private int tableNumber;
 	private int capacity;
 	private boolean isReserved;
+
+	public Table(int tableNumber, int Seats) {
+		// TODO Auto-generated constructor stub
+	}
+
+	// @return the tableNumber
+	public int getTableNumber() {
+		return tableNumber;
+	}
+
+	// @param tableNumber the tableNumber to set
+	public void setTableNumber(int tableNumber) {
+		this.tableNumber = tableNumber;
+	}
+
+	// @return the capacity
+	public int getCapacity() {
+		return capacity;
+	}
+
+	// @param capacity the capacity to set
+	public void setCapacity(int capacity) {
+		this.capacity = capacity;
+	}
+
+	// @return the isReserved
+	public boolean isReserved() {
+		return isReserved;
+	}
+
+	// @param isReserved the isReserved to set
+	public void setReserved(boolean isReserved) {
+		this.isReserved = isReserved;
+	}
 }
--- src/main/java/Services/TableReservationService.java
+++ src/main/java/Services/TableReservationService.java
@@ -1,10 +1,33 @@
 package Services;
 
 import Models.Table;
+
 import java.util.ArrayList;
 import java.util.List;
 
-
 public class TableReservationService {
-	
+	private List<Table> tables;
+
+	public TableReservationService() {
+		this.tables = new ArrayList<>();
+		initializeTables();
+	}
+
+	// Initialize All Tables(10) with capabilities of seating 6 people
+	private void initializeTables() {
+		// Loop to initialize all the tables
+		for (int i = 1; i <= 10; i++) {
+			tables.add(new Table(i, 6));
+		}
+	}
+
+	// @return the tables
+	public List<Table> getAllTables() {
+		return tables;
+	}
+
+	// @param tables the tables to set
+	public void initializeTables(List<Table> tables) {
+		this.tables = tables;
+	}
 }
