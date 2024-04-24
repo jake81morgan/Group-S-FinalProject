52db365ade70e78e56fe8c0bcf0ed67509c7d111
Jackson Morgan <jake81morgan@gmail.com>
2024-04-23 23:42:36 -0500

    Update OrderController.java
    initial addition to controller, creates service to control the order processing

--- src/main/java/Controllers/OrderController.java
+++ src/main/java/Controllers/OrderController.java
@@ -1,5 +1,22 @@
 package Controllers;
 
+import java.util.ArrayList;
+
+import Models.Order;
+import Services.OrderProcessingService;
+
 public class OrderController {
+	
+	private OrderProcessingService orderProcessingService;
+	
+	public OrderController() {
+		this.orderProcessingService = new OrderProcessingService();
+	}
+	
+	public OrderController(ArrayList<Order> orders) {
+		this.orderProcessingService = new OrderProcessingService(orders);
+	}
+	
+	
 
 }
