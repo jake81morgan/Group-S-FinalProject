9c3b45261efee8786686aa17a2340eeed0fc5282
evanboyd <evanboyd@mwc-029168.dhcp.missouri.edu>
2024-04-30 16:50:29 -0500

    Created generateInventoryReport and initializeInventory

--- src/main/java/Services/InventoryManagementService.java
+++ src/main/java/Services/InventoryManagementService.java
@@ -12,6 +12,17 @@ public class InventoryManagementService {
 		this.inventory = new ArrayList<Ingredient>();
 	}
 	
+    public void initializeInventory() {
+        for (Ingredient ingredient : inventory) {
+            // Set quantity to 100
+            ingredient.setQuantity(100);
+            // Set supplier to "Sysco"
+            ingredient.setSupplier("Sysco");
+            // Set threshold to 25
+            ingredient.setOrderThreshold(25);
+        }
+    }
+	
 	public void reorderInventory() {
 		for (Ingredient ingredient : inventory) {
 			if(ingredient.getQuantity() < ingredient.getOrderThreshold()) {
@@ -20,8 +31,18 @@ public class InventoryManagementService {
 			}
 		}
 		
-	}	
+	}
 	
+	public String generateInventoryReport() {
+        StringBuilder report = new StringBuilder();
+        report.append("Inventory Report:\n");
+        for (Ingredient ingredient : inventory) {
+            report.append("Ingredient: ").append(ingredient.getName()).append(", Quantity: ")
+                    .append(ingredient.getQuantity()).append(", Supplier: ").append(ingredient.getSupplier()).append("\n");
+        }
+        return report.toString();
+    }
+
 	public ArrayList<Ingredient> getInventory() {
 		return inventory;
 	}
