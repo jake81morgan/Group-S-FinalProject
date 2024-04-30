f4b6f67d9d98a5f0fe61750047535684e5025df6
evanboyd <evanboyd@mwc-029168.dhcp.missouri.edu>
2024-04-30 16:43:34 -0500

    Created reorderInventory

--- src/main/java/Services/InventoryManagementService.java
+++ src/main/java/Services/InventoryManagementService.java
@@ -1,5 +1,35 @@
 package Services;
 
+import java.util.ArrayList;
+
+import Models.Ingredient;
+
 public class InventoryManagementService {
 
+	private ArrayList<Ingredient> inventory;
+	
+	public InventoryManagementService() {
+		this.inventory = new ArrayList<Ingredient>();
+	}
+	
+	public void reorderInventory() {
+		for (Ingredient ingredient : inventory) {
+			if(ingredient.getQuantity() < ingredient.getOrderThreshold()) {
+				System.out.println("Placing order for " + 100 + " of " + ingredient.getName() + " from supplier: " + ingredient.getSupplier());
+				ingredient.setQuantity(ingredient.getQuantity() + 100);
+			}
+		}
+		
+	}	
+	
+	public ArrayList<Ingredient> getInventory() {
+		return inventory;
+	}
+
+	public void setInventory(ArrayList<Ingredient> inventory) {
+		this.inventory = inventory;
+	}
+	
+	
+	
 }
