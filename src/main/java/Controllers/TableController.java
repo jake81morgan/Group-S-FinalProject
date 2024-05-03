4581f7d46038ab5acf7a33a44a2442e926e06ad5
joeyg <joeyg@Joey_Desktop>
2024-05-03 13:56:32 -0500

    updated TableController class

--- src/main/java/Controllers/TableController.java
+++ src/main/java/Controllers/TableController.java
@@ -3,23 +3,42 @@ package Controllers;
 import Models.Table;
 import Services.TableReservationService;
 
+import java.time.DayOfWeek;
+import java.time.LocalTime;
 import java.util.List;
 
 public class TableController {
-	private TableReservationService TableReservationService;
+    private TableReservationService tableReservationService;
 
-	// @return the getAllTables
-	public List<Table> getAllTables() {
-		return TableReservationService.getAllTables();
-	}
-	// @return the getTableByNumber
-		public Table getTableByNumber(int tableNumber) {
-			return TableReservationService.getTableByNumber(tableNumber);
-		}
+    public void setTableReservationService(TableReservationService tableReservationService) {
+        this.tableReservationService = tableReservationService;
+    }
 
-	// @param tableReservationService the tableReservationService to set
-	public void setTableReservationService(TableReservationService tableReservationService) {
-		this.TableReservationService = tableReservationService;
-	}
+    public List<Table> getAllTables() {
+        return tableReservationService.getAllTables();
+    }
 
+    public Table getTableByNumber(int tableNumber) {
+        return tableReservationService.getTableByNumber(tableNumber);
+    }
+
+    public boolean bookTable(int tableNumber, int customerCount, String customerName, DayOfWeek reservationDate, LocalTime reservationTime) {
+        return tableReservationService.bookTable(tableNumber, customerCount, customerName, reservationDate, reservationTime);
+    }
+
+    public boolean cancelReservation(String customerName) {
+        return tableReservationService.cancelReservation(customerName);
+    }
+
+    public void availableTables() {
+        tableReservationService.availableTables();
+    }
+
+    public void confirmReservation(boolean tableIsReserved, int tableNumber) {
+        tableReservationService.confirmReservation(tableIsReserved, tableNumber);
+    }
+
+    public String viewReservationDetails(String customerName) {
+        return tableReservationService.viewReservationDetails(customerName);
+    }
 }
