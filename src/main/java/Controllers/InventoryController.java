bea8956f5219454e07a91bd15bbccb4b48747da1
joeyg <joeyg@Joey_Desktop>
2024-05-06 14:46:39 -0500

    Auto-Formatted each class

--- src/main/java/Controllers/InventoryController.java
+++ src/main/java/Controllers/InventoryController.java
@@ -3,23 +3,23 @@ package Controllers;
 import Services.InventoryManagementService;
 
 public class InventoryController {
-	
+
 	private InventoryManagementService inventoryService;
-	
+
 	public InventoryController() {
 		this.inventoryService = new InventoryManagementService();
 	}
-	
+
 	public void initializeInventory() {
 		inventoryService.initializeInventory();
 	}
-	
+
 	public void reOrderInventory() {
 		inventoryService.reorderInventory();
 	}
-	
+
 	public String generateInventoryReport() {
 		return inventoryService.generateInventoryReport();
 	}
-	
+
 }
